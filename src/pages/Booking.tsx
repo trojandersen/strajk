99c20d0a60fb64c2af810c526b5aa785a7dfb724
Troj Andersen <trojandersen@gmail.com>
2025-01-14 15:20:16 +0100

    styling, tag change

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -15,130 +15,131 @@ const Booking: React.FC = () => {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="bg-[#FFF4F1] flex flex-col items-center"
-    >
-      <img src={logo} alt="logo" />
-      <h1 className="font-BebasNeune text-customRed text-[80px] tracking-wide">
-        BOOKING
-      </h1>
-      <fieldset className="space-y-3 mb-3">
-        <legend className="text-lg font-semibold text-customPurple">
-          WHEN, WHAT & WHO
-        </legend>
-        <section className="grid grid-cols-2 gap-4">
-          <div>
-            <label
-              htmlFor="date"
-              className="block text-sm font-medium text-customPurple"
-            >
-              DATE
-            </label>
-            <input
-              id="date"
-              type="text"
-              defaultValue="3 dec"
-              className="mt-1 px-3 py-2 border rounded-lg focus:ring-pink-500 focus:border-pink-500"
-            />
-          </div>
-
-          <div>
-            <label
-              htmlFor="time"
-              className="block text-sm font-medium text-customPurple"
-            >
-              TIME
-            </label>
-            <input
-              id="time"
-              type="text"
-              defaultValue="21.00"
-              className="w-full mt-1 px-3 py-2 border rounded-lg focus:ring-pink-500 focus:border-pink-500"
-            />
-          </div>
-        </section>
-
-        <label
-          htmlFor="bowlers"
-          className="block text-sm font-medium text-customPurple"
-        >
-          NUMBER OF AWESOME BOWLERS
-        </label>
-        <input
-          id="bowlers"
-          type="text"
-          defaultValue="3 pers"
-          className="w-full mt-1 px-3 py-2 border rounded-lg focus:ring-pink-500 focus:border-pink-500"
-        />
-
-        <label
-          htmlFor="lanes"
-          className="block text-sm font-medium text-customPurple"
-        >
-          NUMBER OF LANES
-        </label>
-        <input
-          id="lanes"
-          type="text"
-          defaultValue="1 lane"
-          className="w-full mt-1 px-3 py-2 border rounded-lg focus:ring-pink-500 focus:border-pink-500"
-        />
-      </fieldset>
-
-      <fieldset className="space-y-3 mb-5">
-        <legend className="text-lg font-semibold text-customPurple">
-          SHOES
-        </legend>
-        {shoeSizes.map((size, index) => (
-          <section key={index} className="">
-            <div className="flex flex-col">
+    <main className="bg-[#FFF4F1]">
+      <section className="flex flex-col items-center">
+        <img src={logo} alt="logo" />
+        <h1 className="font-BebasNeune text-customRed text-[80px] tracking-wide">
+          BOOKING
+        </h1>
+      </section>
+      <form onSubmit={handleSubmit} className="flex flex-col items-center">
+        <fieldset className="space-y-3 mb-3">
+          <legend className="text-lg font-semibold text-customPurple">
+            WHEN, WHAT & WHO
+          </legend>
+          <section className="grid grid-cols-2 gap-4">
+            <div>
               <label
-                htmlFor={`size-${index}`}
-                className=" text-sm font-medium text-customPurple"
+                htmlFor="date"
+                className="block text-sm font-medium text-customPurple"
               >
-                SHOE SIZE {index + 1}
+                DATE
               </label>
-              <div className="flex gap-5">
-                <input
-                  id={`size-${index}`}
-                  type="text"
-                  value={size}
-                  onChange={(e) =>
-                    setShoeSizes(
-                      shoeSizes.map((s, i) =>
-                        i === index ? e.target.value : s
-                      )
-                    )
-                  }
-                  className="px-3 py-2 border rounded-lg w-full"
-                />
-                <button
-                  type="button"
-                  onClick={() => removeShoeSize(index)}
-                  className="px-4 py-1 bg-customRed text-white text-2xl rounded-full  hover:bg-red-500"
-                >
-                  -
-                </button>
-              </div>
+              <input
+                id="date"
+                type="text"
+                defaultValue="3 dec"
+                className="mt-1 px-3 py-2 border rounded-lg focus:ring-pink-500 focus:border-pink-500"
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="time"
+                className="block text-sm font-medium text-customPurple"
+              >
+                TIME
+              </label>
+              <input
+                id="time"
+                type="text"
+                defaultValue="21.00"
+                className="w-full mt-1 px-3 py-2 border rounded-lg focus:ring-pink-500 focus:border-pink-500"
+              />
             </div>
           </section>
-        ))}
+
+          <label
+            htmlFor="bowlers"
+            className="block text-sm font-medium text-customPurple"
+          >
+            NUMBER OF AWESOME BOWLERS
+          </label>
+          <input
+            id="bowlers"
+            type="text"
+            defaultValue="3 pers"
+            className="w-full mt-1 px-3 py-2 border rounded-lg focus:ring-pink-500 focus:border-pink-500"
+          />
+
+          <label
+            htmlFor="lanes"
+            className="block text-sm font-medium text-customPurple"
+          >
+            NUMBER OF LANES
+          </label>
+          <input
+            id="lanes"
+            type="text"
+            defaultValue="1 lane"
+            className="w-full mt-1 px-3 py-2 border rounded-lg focus:ring-pink-500 focus:border-pink-500"
+          />
+        </fieldset>
+
+        <fieldset className="flex flex-col items-center space-y-3 mb-5">
+          <legend className="text-lg font-semibold text-customPurple">
+            SHOES
+          </legend>
+          {shoeSizes.map((size, index) => (
+            <section key={index}>
+              <div className="flex flex-col">
+                <label
+                  htmlFor={`size-${index}`}
+                  className=" text-sm font-medium text-customPurple"
+                >
+                  SHOE SIZE {index + 1}
+                </label>
+                <div className="flex gap-5">
+                  <input
+                    id={`size-${index}`}
+                    type="text"
+                    value={size}
+                    onChange={(e) =>
+                      setShoeSizes(
+                        shoeSizes.map((s, i) =>
+                          i === index ? e.target.value : s
+                        )
+                      )
+                    }
+                    className="px-3 py-2 border rounded-lg w-full"
+                  />
+                  <button
+                    type="button"
+                    onClick={() => removeShoeSize(index)}
+                    className="px-4 py-1 bg-customRed text-white text-2xl rounded-full  hover:bg-red-500"
+                  >
+                    -
+                  </button>
+                </div>
+              </div>
+            </section>
+          ))}
+          <button
+            type="button"
+            onClick={addShoeSize}
+            className="px-3 py-1 bg-customRed text-white text-2xl rounded-full hover:bg-red-500"
+          >
+            +
+          </button>
+        </fieldset>
         <button
-          type="button"
-          onClick={addShoeSize}
-          className="px-3 py-1 bg-customRed text-white text-2xl rounded-full hover:bg-red-500"
+          type="submit"
+          className="py-3 px-12 mb-1 font-WorkSans bg-customRed text-white font-bold rounded-lg tracking-widest hover:bg-red-500"
         >
-          +
+          STRIIIIIIKE!
         </button>
-      </fieldset>
-      <button
-        type="submit"
-        className="py-3 px-10 mb-1 font-WorkSans bg-customRed text-white font-bold rounded-lg hover:bg-red-500"
-      >
-        STRIIIIIIKE!
-      </button>
-    </form>
+      </form>
+    </main>
   );
 };
 
--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.svg";
 
 const Confirmation: React.FC = () => {
   return (
-    <motion.main className="flex flex-col items-center">
+    <motion.main className="bg-[#FFF4F1] flex flex-col items-center">
       <section className="flex flex-col items-center">
         <img src={logo} alt="logo" />
         <h1 className="font-BebasNeune text-customRed  text-[80px] tracking-wide">
@@ -11,7 +11,7 @@ const Confirmation: React.FC = () => {
         </h1>
       </section>
       <section></section>
-      <button className="font-WorkSans bg-customRed border-r-4 p-5 text-white">
+      <button className="py-3 px-12 font-WorkSans bg-customRed text-white font-bold tracking-widest rounded-lg hover:bg-red-500">
         SWEET, LETS GO!
       </button>
     </motion.main>
