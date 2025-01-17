dfc2daeee0de28f2812e8691ed37c87b79a618e0
Troj Andersen <trojandersen@gmail.com>
2025-01-17 15:00:45 +0100

    proper positioning & scroll

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -1,13 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import logo from "../assets/logo.svg";
 
 const Booking: React.FC = () => {
   const [shoeSizes, setShoeSizes] = useState<string[]>(["Euro 44"]);
+  const lastShoeSizeRef = useRef<HTMLDivElement | null>(null);
 
   const addShoeSize = () => setShoeSizes([...shoeSizes, "Euro 44"]);
   const removeShoeSize = (index: number) => {
     setShoeSizes(shoeSizes.filter((_, i) => i !== index));
   };
+
+  useEffect(() => {
+    if (lastShoeSizeRef.current) {
+      lastShoeSizeRef.current.scrollIntoView({
+        behavior: "instant",
+        block: "end",
+      });
+    }
+  }, [shoeSizes]);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -15,7 +25,7 @@ const Booking: React.FC = () => {
   };
 
   return (
-    <main className="bg-[#FFF4F1]">
+    <main className="bg-[#FFF4F1] flex flex-col items-center">
       <section className="flex flex-col items-center">
         <img src={logo} alt="logo" />
         <h1 className="font-BebasNeune text-customRed text-[80px] tracking-wide">
@@ -23,7 +33,7 @@ const Booking: React.FC = () => {
         </h1>
       </section>
       <form onSubmit={handleSubmit} className="flex flex-col items-center">
-        <fieldset className="space-y-3 mb-3">
+        <fieldset className="space-y-3 mb-3 w-full">
           <legend className="text-lg font-semibold text-customPurple">
             WHEN, WHAT & WHO
           </legend>
@@ -54,7 +64,7 @@ const Booking: React.FC = () => {
                 id="time"
                 type="text"
                 defaultValue="21.00"
-                className="w-full mt-1 px-3 py-2 border rounded-lg focus:ring-pink-500 focus:border-pink-500"
+                className="mt-1 px-3 py-2 border rounded-lg focus:ring-pink-500 focus:border-pink-500"
               />
             </div>
           </section>
@@ -86,16 +96,16 @@ const Booking: React.FC = () => {
           />
         </fieldset>
 
-        <fieldset className="flex flex-col items-center space-y-3 mb-5">
+        <fieldset className="space-y-3 mb-5 w-full">
           <legend className="text-lg font-semibold text-customPurple">
             SHOES
           </legend>
           {shoeSizes.map((size, index) => (
             <section key={index}>
-              <div className="flex flex-col">
+              <div>
                 <label
                   htmlFor={`size-${index}`}
-                  className=" text-sm font-medium text-customPurple"
+                  className="text-sm font-medium text-customPurple"
                 >
                   SHOE SIZE {index + 1}
                 </label>
@@ -116,7 +126,7 @@ const Booking: React.FC = () => {
                   <button
                     type="button"
                     onClick={() => removeShoeSize(index)}
-                    className="px-4 py-1 bg-customRed text-white text-2xl rounded-full  hover:bg-red-500"
+                    className="px-4 py-1 bg-customRed text-white text-2xl rounded-full hover:bg-red-500"
                   >
                     -
                   </button>
@@ -124,20 +134,23 @@ const Booking: React.FC = () => {
               </div>
             </section>
           ))}
-          <button
-            type="button"
-            onClick={addShoeSize}
-            className="px-3 py-1 bg-customRed text-white text-2xl rounded-full hover:bg-red-500"
-          >
-            +
-          </button>
+          <div className="flex justify-center">
+            <button
+              type="button"
+              onClick={addShoeSize}
+              className="px-3 py-1 bg-customRed text-white text-2xl rounded-full hover:bg-red-500"
+            >
+              +
+            </button>
+          </div>
         </fieldset>
         <button
           type="submit"
-          className="py-3 px-12 font-WorkSans bg-customRed text-white font-bold rounded-lg tracking-widest hover:bg-red-500"
+          className="mb-2 py-3 px-12 font-WorkSans bg-customRed text-white font-bold rounded-lg tracking-widest hover:bg-red-500"
         >
           STRIIIIIIKE!
         </button>
+        <div ref={lastShoeSizeRef} />
       </form>
     </main>
   );
